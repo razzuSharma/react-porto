c95d20530482af2139d6692178774adbe90ac123
razzuSharma <razzusharma008@gmail.com>
2024-07-02 23:38:35 +0545

    refactor: refactoring all the ui components

--- src/navbar/Navbar.jsx
+++ src/navbar/Navbar.jsx
@@ -1,10 +1,10 @@
 import React from "react";
 const Navbar = () => {
   return (
-    <nav className="sticky top-0 z-10 bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg border-b border-gray-200">
+    <nav className="sticky top-0 z-10  bg-white bg-opacity-50 backdrop-filter backdrop-blur-lg border-b border-gray-200">
       <div className="max-w-5xl mx-auto px-4">
         <div className="flex items-center justify-between h-16">
-          <span className="text-2xl text-gray-900 font-semibold">Logo</span>
+          <a className="text-2xl text-gray-900 font-semibold" href="/">RSD</a>
           <div className="flex space-x-8 text-gray-900">
             <a href="/projects">Projects</a>
             <a href="/contact">Contact</a>
